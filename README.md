4f9f2303e266ebb5ec760f139676089c2afc20e8
amarnathcsFullStack <amarnathcs123@gmail.com>
2026-08-06 18:52:54 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# mini-zoo-animal-service
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/amarnathcsFullStack/mini-zoo-animal-service)
---
+++ index.js
@@ -0,0 +1,43 @@
+// run `node index.js` in the terminal
+const express = require("express");
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
+const animal = {
+  name: "Tiger",
+  species: "Tiger",
+  age: 5,
+  bellySize: 3
+};
+app.get("/", (req, res) => {
+  res.send("Welcome to Animal Service");
+});
+
+app.get("/animals", (req, res) => {
+  res.json(animal);
+});
+
+app.post("/animals/1/feed", (req, res) => {
+  if (animal.bellySize >= 10) {
+    return res.status(400).json({
+      message: "Tiger is already full"
+    });
+  }
+
+  animal.bellySize += 1;
+
+  res.json({
+    message: "Tiger was fed",
+    animal: animal
+  });
+});
+
+
+app.listen(3000, () => {
+  console.log("Animal Service is running on port 3000");
+});
