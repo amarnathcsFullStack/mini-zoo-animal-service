83410bcc777ba91f029bb69359bda17d3b97843f
amarnathcsFullStack <amarnathcs123@gmail.com>
2026-08-08 01:33:50 +0530

    to see db

--- index.js
+++ index.js
@@ -87,6 +87,11 @@ async function startServer() {
       bellySize: animal[4]
     });
   });
+  app.get("/animals/db", (req, res) => {
+    const result = db.exec("SELECT * FROM animals");
+  
+    res.json(result);
+  });
 
   // POST /animals/1/feed
   app.post("/animals/1/feed", async (req, res) => {
